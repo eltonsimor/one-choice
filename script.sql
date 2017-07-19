0a06cef210f7fc5e1b10727123280081b0ed2d7f
Elton Moraes <eltonsimor@gmail.com>
2017-07-19 03:11:05 -0300

    ajustes no script sql

--- script.sql
+++ script.sql
@@ -1,11 +1,10 @@
 CREATE SCHEMA `vianuvem` ;
 
-CREATE TABLE IF NOT EXISTS `vianuvem`.`Lists` (
+CREATE TABLE IF NOT EXISTS `vianuvem`.`lists` (
   `pk` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(70) NOT NULL,
   PRIMARY KEY (`pk`))
 ENGINE = InnoDB;
-
 
 CREATE TABLE IF NOT EXISTS `vianuvem`.`orders` (
   `pk` INT NOT NULL AUTO_INCREMENT,
@@ -19,13 +18,13 @@ CREATE TABLE IF NOT EXISTS `vianuvem`.`orders` (
   INDEX `fk_orders_Lists_idx` (`lists_pk` ASC),
   CONSTRAINT `fk_orders_Lists`
     FOREIGN KEY (`lists_pk`)
-    REFERENCES `mydb`.`Lists` (`pk`)
+    REFERENCES `vianuvem`.`lists` (`pk`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 use vianuvem;
 
-INSERT INTO Lists (name) values("Lista Para Churrasco")
+INSERT INTO lists (name) values("Lista Para Churrasco");
 
 insert into orders (name, quantity, price, name_image, marked, lists_pk) values("Picanha", 30, 39.80, "picanha.jpg", 'N', 1);
